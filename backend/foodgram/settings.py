8e0516db1cbd55ba2f0bf550d67678b19c50aa4a
Eugene-Maslov <Eugene.Maslov@yandex.ru>
2023-05-09 15:01:51 +0300

    Na review

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -61,13 +61,12 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-#        'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', #
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPageNumberPagination',
     'PAGE_SIZE': 6,
 }
 
