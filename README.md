938690ddafc4e25bba9365c83e397b726b1762d3
Eugene-Maslov <Eugene.Maslov@yandex.ru>
2023-05-17 11:52:53 +0300

    Debugging

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# praktikum_new_diplom
+![Workflow status](https://github.com/Eugene-Maslov/foodgram-project-react/actions/workflows/foodgram_workflow.yml/badge.svg)
+
+# Foodgram - «Продуктовый помощник»
--- backend/users/views.py
+++ backend/users/views.py
@@ -12,7 +12,7 @@ from users.models import Follow, User
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    # permission_classes = (IsAuthenticatedOrReadOnly,)
     filter_backends = (filters.SearchFilter,)
     pagination_class = CustomPageNumberPagination
     search_fields = ('username',)
