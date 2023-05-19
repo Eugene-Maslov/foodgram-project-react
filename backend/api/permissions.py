b4b1f73c3dc22decea0a021d190fdf3feeb672ad
Eugene-Maslov <Eugene.Maslov@yandex.ru>
2023-05-19 21:42:32 +0300

    Permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -7,8 +7,9 @@ class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
                 or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (request.user.is_staff or request.user.is_superuser
-                or obj.author == request.user)
+        return (request.method in permissions.SAFE_METHODS
+                or (request.user.is_staff or request.user.is_superuser
+                or obj.author == request.user))
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
